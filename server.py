9ced2ba0e2bf18203464c7fe3fac173c38d992e8
Hermine <hermineavagyan@yahoo.com>
2021-10-25 00:56:47 -0700

    USER_CR project first version. needs to be worked on redirecting from theindex page to add a new user page

---
+++ server.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request, redirect
+# import the class from friend.py
+from user import User
+app = Flask(__name__)
+@app.route("/")
+def index():
+    # call the get all classmethod to get all friends
+    users = User.get_all()
+    print(users)
+    return render_template("readAll.html", all_users = users)
+
+@app.route('/create_user', methods=["POST"])
+def create_user():
+    # First we make a data dictionary from our request.form coming from our template.
+    # The keys in data need to line up exactly with the variables in our query string.
+    data = {
+        "fname": request.form["fname"],
+        "lname" : request.form["lname"],
+        "email" : request.form["email"]
+    }
+    # We pass the data dictionary into the save method from the Friend class.
+    User.save(data)
+    # Don't forget to redirect after saving to the database.
+    return redirect("create.html")
+    
+            
+if __name__ == "__main__":
+    app.run(debug=True)
